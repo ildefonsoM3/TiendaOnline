9e1917dd6dc7a95f51593b4e2de297747d7a4183
Ponchom3 <ponchom3@gmail.com>
2016-11-27 14:28:53 -0600

    Se trabajó en el lado del administrador pero me sale un error, no funciona

--- admin/pedidos.php
+++ admin/pedidos.php
@@ -3,34 +3,42 @@
     
 $conexion = mysqli_connect("localhost", "tienda", "tienda", "tiendaonline");
 mysqli_set_charset($conexion, "utf8");
-$peticion = "SELECT * FROM pedidos ORDER BY estado, fecha ASC";
+$peticion = 
+    "SELECT pedidos.id AS idpedido, fecha, estado, nombre, apellidos FROM pedidos LEFT JOIN clientes ON pedidos.idcliente = clientes.id ORDER BY estado,fecha ASC";
 $resultado = mysqli_query($conexion, $peticion);
 
 while($fila = mysqli_fetch_array($resultado)){
     $estado = $fila['estado'];
-    if($estado == 0){
-        $elEstadoEs = "No Servido";
-        
-    }else {
-        $elEstadoEs = "Servido";    
+    
+    switch($estado){
+        case 0: $elEstadoEs = "Pendiente";
+            break;
+        case 1: $elEstadoEs = "Servido";
+            break;
+        case 1: $elEstadoEs = "Cancelado";
+            break;
     }
     
-    echo '
-        <tr ';
-            if($estado == 0){
-                echo ' style="background: rgb(255,200,200);"';
-                
-            }else {
-                echo ' style="background: rgb(200,255,200);"';        
+    echo '<tr ';            
+            switch($estado){
+                case 0: echo ' style="background: rgb(255,200,200);"';
+                    break;
+                case 1: echo ' style="background: rgb(200,255,200);"';
+                    break;
+                case 1: echo ' style="background: rgb(255,255,200);"';
+                    break;
             }
         echo '>
-            <td>'.$fila['idcliente'].'</td>
+            <td>'.$fila['nombre']." ".$fila['apellidos'].'</td>
             <td>'.date("M d Y H:i:s", $fila['fecha']).'</td>
-            <td>'.$fila['estado'].'</td>
-        </tr>
-    ';
+            <td>'.$elEstadoEs.'</td>
+            <td><a href="gestionpedido.php?id='.$fila['idpedido'].'"><button>Gestionar</button></a></td>
+            <td><a href="../php/pedidoservido.php?id='.$fila['idpedido'].'"><button>Pedido Servido</button></a></td>
+            <td><a href="../php/cancelarpedido.php?id='.$fila['idpedido'].'"><button>Cancelar Pedido</button></a></td>
+        </tr>';
 }
 mysqli_close($conexion);
 
 ?>
 </table>
+
