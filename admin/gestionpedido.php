3fed091824b9ed15d2c069579fb43808e923d836
Ponchom3 <ponchom3@gmail.com>
2016-11-28 23:27:22 -0600

    Tengo errores en la parte de la administración que posteriormente estaré trabajando en ello

---
+++ admin/gestionpedido.php
@@ -0,0 +1,12 @@
+<table>
+    <tr>
+        <td>
+            <h1>
+                Nombre de la Tienda
+            </h1>
+            <p>
+                Dirección de la tienda
+            </p>
+        </td>
+    </tr>
+</table>
